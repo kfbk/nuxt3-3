f897c954a895e741e16954942be2d5723d61a22e
DESKTOP-DC91P5K\user <satou1497@gmail.com>
2023-04-26 17:54:26 +0900

    first

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["nuxt-microcms-module"],
+  microCMS: {
+    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: process.env.MICROCMS_API_KEY,
+  },
+})
